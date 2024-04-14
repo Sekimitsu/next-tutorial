83b66842df664b3e0edd77b1d1884cf85818aad7
Sekimitsu <sekimitsu0511@gmail.com>
2024-04-14 22:12:19 +0900

    Underitem

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Main } from "../../component/Main";
+import { Underitem } from "../../component/Underitem";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -51,7 +51,7 @@ export default function Home() {
           />
         </div>
 
-        <Main />
+        <Underitem />
 
       </main>
     </>
