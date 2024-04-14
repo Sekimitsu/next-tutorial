6a0fd578f5ef87ddb2f00b80dfc101c81151388a
Sekimitsu <sekimitsu0511@gmail.com>
2024-04-14 22:40:03 +0900

    Links index

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Vercel } from "../../component/Vercel"
 import { Links } from "../../component/Links";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +17,29 @@ export default function Home() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
 
-        <Vercel />
+      <div className={styles.description}>
+          <p>
+            Get started by editing&nbsp;
+            <code className={styles.code}>src/pages/about.js</code>
+          </p>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
 
         <div className={styles.center}>
           <Image
